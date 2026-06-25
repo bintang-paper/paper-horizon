be476aa1e377b6ad056c36375e9b09df3e72eee0
Bintang F. <bintang.fajarianto@paper.id>
2026-06-25 11:43:48 +0700

    feat: force app orientation to potrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async' show runZonedGuarded;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:paper_horizon/app.dart';
 import 'package:paper_horizon/config/env/env.dart';
 import 'package:paper_horizon/core/di/di.dart';
@@ -9,6 +10,13 @@ import 'package:paper_horizon/i18n/strings.g.dart';
 void configureApp(EnvFlavor flavor) {
   runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
+
+    // Force portrait orientation
+    await SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     LocaleSettings.setLocale(AppLocale.id); // Force Indonesian locale
 
     // DI
