d8d66ad31dce3bbab6a378bbcf8873be7e841d3e
Bintang F. <bintang.fajarianto@paper.id>
2026-06-26 00:13:22 +0700

    feat: setup horizon images source of truth

---
+++ lib/shared/horizon_images.dart
@@ -0,0 +1,10 @@
+final class HorizonImages {
+  const HorizonImages._();
+
+  static const String logo = 'assets/images/img-logo.webp';
+  static const String logoDark = 'assets/images/img-logo-dark.webp';
+
+  /// onboarding
+  static const String backgroundIntroduction =
+      'assets/images/onboarding/img-background-introduction.webp';
+}
